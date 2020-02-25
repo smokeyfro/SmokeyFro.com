d8fb45a54fa1266eaff9fc32e6736336fdd2de2c
Chris Rault <chris@smokeyfro.com>
2020-02-25 17:13:15 +0200

    Try that again shall we

--- src/pages/work/Index.vue
+++ src/pages/work/Index.vue
@@ -5,22 +5,8 @@
 		<template slot="secondary-nav">
 			<NavWork />
 		</template>
-		<template slot="bottom-shelf">
-			<article class="mb-2" v-for="edge in $page.tuts.edges" :key="edge.node.id" v-if="edge.node.slug === 'winefolly'">
-				<g-link :to="`${post.path}`" class="image">
-					<div class="card-image" v-if="post.fields.image.path">
-						<Browser :image="imageUrl" />
-					</div>
-				</g-link>
-				<div class="summary">
-					<h2 class="mt-5 text-2xl">{{ post.title }}</h2>
-					<p class="mb-3">{{ post.fields.excerpt }}</p>
-					<p class="mb-3">{{ post.fields.status }}</p>
-					<p class="mb-0"><g-link :to="`${post.path}`" class="text-base">Project details</g-link></p>
-				</div>
-			</article>
-			
-			<div class="grid gap-8 mx-6 mt-10 mb-10 md:gap-8 grid-cols1 md:grid-cols-2 lg:grid-cols-3 md:mx-10 lg:mx-20 ">
+		<template slot="bottom-shelf">	
+			<div class="grid gap-8 mx-6 mt-10 mb-10 md:gap-8 grid-cols1 lg:grid-cols-2 xl:grid-cols-3 md:mx-10 lg:mx-20 ">
 				<ItemWork :post="edge.node" v-for="edge in $page.posts.edges" :key="edge.node.id" />
 			</div>
 		</template>

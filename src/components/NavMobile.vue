228c128630180bd060fc6553ef82b11350766056
Chris Rault <chris@smokeyfro.com>
2020-02-27 16:41:11 +0200

    Removed leftover nav stuff

--- src/components/NavMobile.vue
+++ src/components/NavMobile.vue
@@ -19,43 +19,22 @@
       <span>Contact</span>
     </g-link>
     <a href="#" class="relative opacity-100" aria-label="Explore"
-        @click="toggleVisibility"
-        @keydown.space.exact.prevent="toggleVisibility"
-        @keydown.esc.exact="hideDropdown"
-        @keydown.shift.tab="hideDropdown"
-        @keydown.up.exact.prevent="startArrowKeys"
-        @keydown.down.exact.prevent="startArrowKeys">
-        <span  v-if="isVisible" class="block px-2 close-nav"><svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" viewBox="0 0 24 24"><defs/><path d="M18 6L6 18M6 6l12 12"/></svg></span>
+        @click="toggleVisibility">
+        <span v-if="isVisible" class="block px-2 close-nav"><svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" viewBox="0 0 24 24"><defs/><path d="M18 6L6 18M6 6l12 12"/></svg></span>
         <span v-else class="block px-2 "><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="1"/><circle cx="19" cy="12" r="1"/><circle cx="5" cy="12" r="1"/></svg></span>
         <transition name="dropdown-fade">
         <div class="absolute mobile-links" v-on-clickaway="hideDropdown" v-if="isVisible" ref="dropdown">
             <nav>
-                <g-link to="/themes" aria-label="Themes"
-                    @keydown.up.exact.prevent=""
-                    @keydown.tab.exact="focusNext(false)"
-                    @keydown.down.exact.prevent="focusNext(true)"
-                    @keydown.esc.exact="hideDropdown">
+                <g-link to="/themes" aria-label="Themes" @keydown.esc.exact="hideDropdown">
                     <span>Themes</span>
                 </g-link>
-                <g-link to="/journal" aria-label="Journal"
-                    @keydown.up.exact.prevent=""
-                    @keydown.tab.exact="focusNext(false)"
-                    @keydown.down.exact.prevent="focusNext(true)"
-                    @keydown.esc.exact="hideDropdown">
+                <g-link to="/journal" aria-label="Journal" @keydown.esc.exact="hideDropdown">
                     <span>Journal</span>
                 </g-link>
-                <g-link to="/tutorials" aria-label="Tutorials"
-                    @keydown.up.exact.prevent=""
-                    @keydown.tab.exact="focusNext(false)"
-                    @keydown.down.exact.prevent="focusNext(true)"
-                    @keydown.esc.exact="hideDropdown">
+                <g-link to="/tutorials" aria-label="Tutorials" @keydown.esc.exact="hideDropdown">
                     <span>Tutorials</span>
                 </g-link>
-                <g-link to="/subscribe" aria-label="Subscribe"
-                    @keydown.up.exact.prevent=""
-                    @keydown.tab.exact="focusNext(false)"
-                    @keydown.down.exact.prevent="focusNext(true)"
-                    @keydown.esc.exact="hideDropdown">
+                <g-link to="/subscribe" aria-label="Subscribe" @keydown.esc.exact="hideDropdown">
                     <span>Subscribe</span>
                 </g-link>
             </nav>
@@ -84,28 +63,7 @@ export default {
         hideDropdown() {
             this.isVisible = false
             this.focusedIndex = 0
-        },
-        startArrowKeys() {
-            if (this.isVisible) {
-                // this.$refs.about.focus()
-                this.$refs.dropdown.children[0].children[0].focus()
-            }
-        },
-        focusPrevious(isArrowKey) {
-            this.focusedIndex = this.focusedIndex - 1
-            if (isArrowKey) {
-                this.focusItem()
-            }
-        },
-        focusNext(isArrowKey) {
-            this.focusedIndex = this.focusedIndex + 1
-            if (isArrowKey) {
-                this.focusItem()
-            }
-        },
-        focusItem() {
-            this.$refs.dropdown.children[this.focusedIndex].children[0].focus()
-        },
+        }
     }
 }
 </script>

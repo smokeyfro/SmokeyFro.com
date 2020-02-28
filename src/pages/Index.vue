bcf8daa8ad592ce5ba89fbe13dc2a51ae144d8a9
Chris Rault <chris@smokeyfro.com>
2020-02-28 09:38:47 +0200

    Last one

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -6,8 +6,8 @@
 			<div>
 				<h1><span class="block sm:hidden">Howdy. I'm Chris</span><span class="hidden sm:block">Hello. My name  <strong>Chris</strong></span></h1>
 				<p>I'm a designer, front-end developer and entrepreneur working remotely from the Garden Route of South Africa.</p>
-				<p>This is my home on the web, where I showcase my work, share my themes, write tutorials and post updates via my journal.</p>
-				<p>As a designer who is passionate about writing clean, performant code, I am able to take an idea from conception to the finished product and have worked with clients of all sizes bringing their ideas to life. Need help with yours? <g-link to="/contact" class title="Learn more about me">Get in touch</g-link>.</p>
+				<p>This is my home on the web where I showcase my work, share my themes, write tutorials and post updates via my journal.</p>
+				<p>As a designer who is passionate about writing clean, performant code, I am able to take an idea from conception to finished product and have worked with clients of all sizes bringing their ideas to life. Need help with yours? <g-link to="/contact" class title="Learn more about me">Get in touch</g-link>.</p>
 			</div>
 		</div>
 		</template>
@@ -23,7 +23,7 @@
 			HomeImage
 		},
 		metaInfo: {
-			title: "",
+			title: "Chris Rault, designer, front-end developer and entrepreneur from South Africa",
 			bodyAttrs: {
 				class: "home two-col"
 			}

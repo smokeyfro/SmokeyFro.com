395629d75aefd86a372c75cfab3e3d07fd077e62
Chris Rault <chris@smokeyfro.com>
2020-03-02 09:46:25 +0200

    Overhauled the talks page to pull from yaml source

--- src/pages/work/Speaking.vue
+++ src/pages/work/Speaking.vue
@@ -6,110 +6,51 @@
 			<h2>Topics I'm current excited about</h2>
 			<p>These are the topics that are currently getting me excited. I'll update these over time as my interests develop.</p>
 			<div class="grid gap-4 md:gap-10 grid-cols1 md:grid-cols-2">
-				<article>
-					<h3>The JAM Stack</h3>
-					<p class="m-0">The JAMstack refers to building static sites (ie, no database or php) using modern tooling and programming practices. It's come along way in recent years and we have more tools available than ever.</p>
-				</article>
-				<article>
-					<h3>The Beans Framework</h3>
-					<p class="m-0">This talk introduces the audience to the Beans framework, what problems it attempts to solve, a breakdown of the various api's that are included and lastly some examples of using Beans in a real-world project. </p>
-				</article>
-				<article>
-					<h3>Bootstrap Business</h3>
-					<p class="m-0">How do you launch your idea when you don't have access to funding? Myself and my business partners bootstrapped a number of online companies, each gaining their own success and failures along the way.</p>
-				</article>
-				<article>
-					<h3>My Startup Journey</h3>
-					<p class="m-0">A brief history of the three digital product companies I helped create, our sweet taste of success, our discent down the rabbit whole, a tale of not giving up and a bunch of solid lessons learnt along the way.</p>
+				<article v-for="({ title, summary }, i) in talks.topics" :key="i">
+					<h3>{{ title }}</h3>
+					<p class="m-0">{{ summary }}</p>
 				</article>
 			</div>
 		</div>
-		<div class="mt-20 mb-1 lg:flex lg:items-center lg:justify-between">
-			<h2 class="m-0 text-sm uppercase">Past Talks</h2>
-			<p class="m-0 ml-2 text-base">Below are some of my past talks from when I was still working with Joomla!.</p>
+		<div class="mt-20 mb-1">
+			<h2 class="m-0 text-xl uppercase">Past Talks</h2>
+			<p class="m-0 mb-3 text-base">Below are some of my past talks from when I was still working with Joomla!.</p>
 		</div>
-		
 		<div class="grid gap-8 pt-6 mb-16 border-t-2 border-black md:gap-10 grid-cols1 md:pt-10">
-			<article>
-				<h3>Content construction with Zoo</h3>
-				<p class="mb-2">For those new to content construction and the Zoo component for Joomla. The talk covered an overview of how content construction works, an overview of Zoo's features and a couple tricks I learnt along the way.</p>
-				<table class="w-full mt-5 text-left bg-white">
-					<thead>
-						<tr>
-							<th>Event</th>
+			<article v-for="({ title, summary, name, date, location, links, event }, i) in talks.talks" :key="i">
+				<h3>{{ title }}</h3>
+				<p class="mb-2">{{ summary }}</p>
+				<div class="hidden mt-6 mb-10 mb-20 overflow-x-auto overflow-y-hidden bg-white border rounded-sm md:block md:mt-10">
+					<table class="w-full max-w-full overflow-hidden text-left rounded-sm">
+						<thead class="text-white bg-black">
+							<th class="rounded-tl-lg">Event</th>
 							<th>Date</th>
 							<th>Location</th>
-							<th></th>
-						</tr>
-					</thead>
-					<tbody>
-						<tr>
-							<td>JoomlaDay South Africa</td>
-							<td>August, 2011</td>
-							<td>Cape Town, South Africa</td>
-							<td><a class="text-sm" href="https://vimeo.com/30687357">Video</a> &middot; <a class="text-sm" href="https://www.slideshare.net/sajoomladay/chris-rault-content-construction-with-zoo?qid=58a5318b-57d7-4846-b869-ae697265862a&v=&b=&from_search=2">Slides</a></td>
-						</tr>
-						<tr>
-							<td>JoomlaDay Malaysia</td>
-							<td>June, 2011</td>
-							<td>Kuala Lumpur, Malaysia</td>
-							<td><a class="text-sm" href="https://www.slideshare.net/connectr/content-construction-with-zoo">Slides</a></td>
-						</tr>
-					</tbody>
-				</table>
-			</article>
-			<article>
-				<h3>Introduction to Morph</h3>
-				<p class="mb-2">An introduction to Morph, the Joomla template framework we created back in 2009. The talk included a demo of the setup process and admin user interface and finally some practical examples of theming using child-themes.</p>
-				<table class="w-full mt-5 text-left bg-white">
-					<thead>
-						<tr>
-							<th>Event</th>
-							<th>Date</th>
-							<th>Location</th>
-							<th></th>
-						</tr>
-					</thead>
-					<tbody>
-						<tr>
-							<td>JoomlaDay South Africa</td>
-							<td>August, 2011</td>
-							<td>Cape Town, South Africa</td>
-							<td><a class="text-sm" href="https://vimeo.com/31361114">Video</a></td>
-						</tr>
-						<tr>
-							<td>JoomlaDay Malaysia</td>
-							<td>June, 2011</td>
-							<td>Kuala Lumpur, Malaysia</td>
-							<td><a class="text-sm" href="https://www.youtube.com/watch?v=byLujAw1Cas">Video</a></td>
-						</tr>
-					</tbody>
-				</table>
-			</article>
-			<article>
-				<h3>Rapid template development with Joomla YUI &amp; jQuery</h3>
-				<p class="mb-2">A two part presentation that covered my tips and trips for Joomla development, using Yahoo's YUI and jQuery.</p>
-				<table class="w-full mt-5 text-left bg-white">
-					<thead>
-						<tr>
-							<th>Event</th>
-							<th>Date</th>
-							<th>Location</th>
-							<th></th>
-						</tr>
-					</thead>
-					<tbody>
-						<tr>
-							<td>CMS Expo</td>
-							<td>December, 2008</td>
-							<td>Denver, CO, USA</td>
-							<td><a class="text-sm" href="https://www.slideshare.net/prothemer/rapid-template-development-part-1-presentation">Part 1</a> &middot; <a class="text-sm" href="https://www.slideshare.net/prothemer/rapid-template-development-part-2-presentation">Part 2</a></td>
-						</tr>
-					</tbody>
-				</table>
+							<th class="rounded-tr-lg"></th>
+						</thead>
+						<tbody>
+							<tr v-for="({ name, date, location, links }, i) in event" :key="i">
+								<td>{{ name }}</td>
+								<td>{{ date }}</td>
+								<td>{{ location }}</td>
+								<td>
+									<a v-for="({ text, url, title }, i) in links" :key="i" class="mr-3 text-sm" :href="url">{{ text}}</a>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+				<div class="block md:hidden ">
+					<ul>
+						<li v-for="({ title, summary, name, date, location, links, event }, i) in talks.talks" :key="i">
+							<h4 class="text-xl font-bold">{{ title }}</h4>
+							<p class="m-0 text-sm">{{ date }} &middot; {{ location }}</p>
+							<p class="m-0 mt-1"><a v-for="({ text, url, title }, i) in links" :key="i" class="mr-3 text-sm" :href="url">{{ text}}</a></p>
+						</li>
+					</ul>
+				</div>
 			</article>
 		</div>
-
 		<template slot="secondary-nav">
 			<NavWork />
 		</template>
@@ -118,9 +59,15 @@
 
 <script>
 import NavWork from "~/components/NavWork.vue";
+import talks from "@/data/talks.yaml";
 export default {
 	components: {
 		NavWork
+	},
+	data() {
+		return {
+			talks
+		};
 	},
 	metaInfo: {
 		title: "",

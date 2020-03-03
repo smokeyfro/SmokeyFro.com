c23a3f43b10ecc2087e3c440e80e7bb159a54aae
Chris Rault <chris@smokeyfro.com>
2020-03-03 15:32:43 +0200

    Home links

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -7,7 +7,8 @@
 				<h1><span class="block sm:hidden">Howdy. I'm Chris</span><span class="hidden sm:block">Hello. My name  <strong>Chris</strong></span></h1>
 				<p>I'm a designer, front-end developer and entrepreneur working remotely from the Garden Route of South Africa.</p>
 				<p>This is my home on the web where I showcase my work, share my themes, write tutorials and post updates via my journal.</p>
-				<p>As a designer who is passionate about writing clean, performant code, I am able to take an idea from conception to finished product and have worked with clients of all sizes bringing their ideas to life. Need help with yours? <g-link to="/contact" class title="Learn more about me">Get in touch</g-link>.</p>
+				<p>As a designer who is passionate about writing clean, performant code, I am able to take an idea from conception to finished product and have worked with clients of all sizes bringing their ideas to life. Need help with yours?</p>
+				<p><g-link to="/work" class title="View my work">View My Work</g-link> <span class="mx-2 text-sm">or</span> <g-link to="/contact" class title="Say hello">Say Hello</g-link></p>
 			</div>
 		</div>
 		</template>

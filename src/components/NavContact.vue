7e31af924e38975be09ba5dc1507ad102c4f4e11
Chris Rault <chris@smokeyfro.com>
2020-03-03 16:57:44 +0200

    Social nav fix

--- src/components/NavContact.vue
+++ src/components/NavContact.vue
@@ -6,7 +6,7 @@
         <a href="skype:chris.rault.tbr?message" title="Send me a message on Skype" class="text-sm external"><span>Skype <img src="/external.svg" class="inline-block w-3 h-3" /></span></a>
         <div class="relative z-50 social-links">
             <a href="#" class="text-sm" @click.prevent="toggleSocial" @keydown.space.exact.prevent="toggleSocial" @keydown.esc.exact="hideDropdown" @keydown.shift.tab="hideDropdown">Social <b v-if="isVisible">-</b><b v-if="isVisible === false">+</b></a>
-            <div class="absolute top-0 right-0 p-6 mt-10 bg-white shadow-lg md:top-auto md:mt-0 rounded-bl-md md:shadow-none md:bg-transparent md:p-0" v-if="isVisible" >
+            <div class="absolute top-0 right-0 p-6 mt-10 bg-white shadow-lg md:mt-10 md:ml-2 md:left-0 md:right-auto md:top-auto md:mt-0 rounded-bl-md md:shadow-none md:bg-transparent md:p-0" v-if="isVisible" >
                 <strong class="block w-full mb-2 text-sm uppercase md:hidden md:mt-6 external">Social</strong>
                 <a href="https://instagram.com/smokeyfro" target="_blank" rel="nofollow noopener" title="" class="block text-sm external social">Instagram <img src="/external.svg" class="inline-block w-3 h-3" /></a>
                 <a href="https://twitter.com/smokeyfro" target="_blank" rel="nofollow noopener" title="" class="block text-sm external social">Twitter <img src="/external.svg" class="inline-block w-3 h-3" /></a>

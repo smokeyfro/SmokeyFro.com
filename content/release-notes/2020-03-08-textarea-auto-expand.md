1c6439bc0d9c09f4040a2b2e5bac45eb9509ef07
Chris Rault <chris@smokeyfro.com>
2020-03-08 16:03:28 +0200

    Implemented release notes

---
+++ content/release-notes/2020-03-08-textarea-auto-expand.md
@@ -0,0 +1,5 @@
+---
+title: Auto-expanding message box on contact form
+summary: Set the message fields to auto expand on the contact form.
+date: 2020-03-08
+---
---
+++ src/components/ReleaseNotes.vue
@@ -0,0 +1,38 @@
+<template>
+<div class="absolute top-0 right-0 notes-wrap">
+    <button>Release Notes</button>
+    <div class="w-56 h-screen p-10 bg-white release-notes">
+    <article v-for="note in $static.notes.edges" :key="note.node.id" class="flex items-start">
+        <div>{{ note.node.date }}</div>
+        <div>
+            <h2 class="font-sans text-sm leading-tight">
+                {{ note.node.title }}
+            </h2>
+            <p v-html="note.node.summary" />`
+        </div> 
+  </article>
+</div>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<static-query>
+query ReleaseNotes {
+	notes: allReleaseNote {
+		edges {
+			node {
+				title
+				summary
+				date(format: "D MMMM, YYYY")
+				path
+                id
+			}
+		}
+	}
+}
+</static-query>

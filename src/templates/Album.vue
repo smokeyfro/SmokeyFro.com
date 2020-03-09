9166c2b89f710ca8a2a1757ba4adf8e014c58c92
Chris Rault <chris@smokeyfro.com>
2020-03-09 15:48:52 +0200

    Trying without the v-if condition

--- src/templates/Album.vue
+++ src/templates/Album.vue
@@ -3,9 +3,9 @@
 		<article>
 			<h1 class="title">{{ $page.post.title }}</h1>
 			<p class="lead" v-html="$page.post.excerpt" />
-            <div class="grid w-full grid-cols-4 gap-px" v-if="$page.post.photos">
+            <div class="grid w-full grid-cols-4 gap-px">
                 <a v-for="(photo, $index) in $page.post.photos" :key="$index" :href="photo.full.src" data-fslightbox="gallery" class="block image">
-                    <g-image :src="`https://smokeyfro.com/` + photo.thumb" width="250" />
+                    <img :src="photo.thumb.src" width="250" />
                 </a>
             </div>
 		</article>

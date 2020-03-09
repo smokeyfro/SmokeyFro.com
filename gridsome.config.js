1f8417e0948309591dbe2e74e0895a857a3d1f51
Chris Rault <chris@smokeyfro.com>
2020-03-09 14:25:41 +0200

    Removed the insta photo collection

--- gridsome.config.js
+++ gridsome.config.js
@@ -81,48 +81,10 @@ module.exports = {
   },
   plugins: [
     {
-      use: '~/src/plugins/gridsome-source-instagram',
-      options: {
-        username: 'smokeyfro',
-        typeName: 'Photo'
-      }
-    },
-    {
       use: 'gridsome-plugin-tailwindcss',
       options: {
         tailwindConfig: './tailwind.config.js',
         purgeConfig: {
-          // whitelist: [
-          //   'pre',
-          //   'code',
-          //   'code-toolbar', 
-          //   'toolbar',
-          //   'toolbar-item',
-          //   'language-javascript', 
-          //   'language-html', 
-          //   'language-css', 
-          //   'language-php',
-          //   'token',
-          //   'operator',
-          //   'keyword',
-          //   'function',
-          //   'function-variable',
-          //   'punctuation',
-          //   'class-name',
-          //   'string',
-          //   'number',
-          //   '.token.keyword',
-          //   '.token.atrule',
-          //   '.token.attr-value',
-          //   'code[class*="language-"]',
-          //   'pre[class*="language-"]'
-          // ],
-          // whitelistPatterns: [
-          //   /code-toolbar/,
-          //   /plyr/,
-          //   /$plyr/,
-          //   /markdown/
-          // ],
           whitelistPatternsChildren: [
             /plyr/,
             /code-toolbar/

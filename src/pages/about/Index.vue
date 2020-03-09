6c195bd9db1adb1a631f81ec4f5561523b1ac116
Chris Rault <chris@smokeyfro.com>
2020-03-09 07:36:13 +0200

    Dark mode tweak

--- src/pages/about/Index.vue
+++ src/pages/about/Index.vue
@@ -2,7 +2,7 @@
 	<Layout :sidebar="true" :top="false" :bottom="true">
 		<div>
 			<div class="about-over">
-				<div class="lg:max-w-screen-md">
+				<div class="content lg:max-w-screen-md">
 					<h1>My Story</h1>
 					<p>My full name is Christopher Nicholas Rault, but I generally only get called Christopher when I'm in trouble. Good ol Chris will do fine. I live off grid in the Garden Route of South Africa.</p>
 					<div class="mb-5 md:flex md:mb-0">

7ec08ca84356060e6a26a07814c708fbe734d1d9
Chris Rault <chris@smokeyfro.com>
2020-03-09 15:39:11 +0200

    Testing setting the image url manually

--- src/templates/Album.vue
+++ src/templates/Album.vue
@@ -5,7 +5,7 @@
 			<p class="lead" v-html="$page.post.excerpt" />
             <div class="grid w-full grid-cols-4 gap-px" v-if="$page.post.photos">
                 <a v-for="(photo, $index) in $page.post.photos" :key="$index" :href="photo.full.src" data-fslightbox="gallery" class="block image">
-                    <g-image :src="photo.thumb" width="250" />
+                    <g-image :src="`https://smokeyfro.com/` + photo.thumb" width="250" />
                 </a>
             </div>
 		</article>

64a734a448af02d72b31f6552d578cfd601286d7
Chris Rault <chris@smokeyfro.com>
2020-03-09 10:08:54 +0200

    Started a log for todays updates

--- src/components/ReleaseNotes.vue
+++ src/components/ReleaseNotes.vue
@@ -1,8 +1,8 @@
 <template>
-<div class="absolute top-0 right-0 notes-wrap">
+<div class="fixed top-0 right-0 mr-4 notes-wrap">
     <button aria-label="Close" v-if="isVisible" class="fixed top-0 right-0 mt-5 mr-5 whitespace-pre hide-notes hover-text-red hover:bg-transparent toggle-notes" @click="toggleVisibility"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M18 6L6 18M6 6l12 12"/></svg></button>
     <button aria-label="Release Notes" v-else class="fixed top-0 right-0 mt-5 mr-8 whitespace-pre show-notes hover-text-red hover:bg-transparent toggle-notes" @click="toggleVisibility"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M18 8A6 6 0 006 8c0 7-3 9-3 9h18s-3-2-3-9M13.73 21a2 2 0 01-3.46 0"/></svg></button>
-    <div v-on-clickaway="hideNotes" v-if="isVisible" class="flex flex-col items-start justify-start h-screen bg-white shadow-lg release-notes">
+    <div v-on-clickaway="hideNotes" v-if="isVisible != false" class="flex flex-col items-start justify-start h-screen bg-white shadow-lg release-notes">
     <h2 class="p-6 m-0 -mt-1 text-lg ">🎉 Recent Updates</h2>
     <div class="overflow-x-auto">
         <article class="box-content p-4 mb-0 odd:bg-gray-200 even:bg-white" v-for="note in $static.notes.edges" :key="note.node.id">
@@ -12,6 +12,7 @@
                     <svg v-if="update.type === 'improvement'" class="inline-block mr-2" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="orange" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="4"/><path d="M1.05 12H7M17.01 12h5.95"/></svg>
                     <svg v-if="update.type === 'new'" class="inline-block mr-2 bg-transparent" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="green" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="4"/><path d="M1.05 12H7M17.01 12h5.95"/></svg>
                     <svg v-if="update.type === 'content'" class="inline-block mr-2 bg-transparent" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="blue" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="4"/><path d="M1.05 12H7M17.01 12h5.95"/></svg>
+                    <svg v-if="update.type === 'fix'" class="inline-block mr-2 bg-transparent" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="red" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="4"/><path d="M1.05 12H7M17.01 12h5.95"/></svg>
                     <p class="m-0 text-sm">{{update.title}}</p>
                 </li>
             </ul>
@@ -19,10 +20,11 @@
     </div>
     <div class="flex items-start self-end justify-start w-full p-4 mt-auto">
         <strong class="mt-1 text-xs uppercase">Key:</strong>
-        <ul class="reset">
-            <li class="flex items-end text-xs text-gray-600"><svg class="inline-block mr-2" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="orange" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="4"/><path d="M1.05 12H7M17.01 12h5.95"/></svg> <span>Improvement</span></li>
+        <ul class="grid w-full grid-cols-1 reset md:grid-cols-2">
             <li class="flex items-end text-xs text-gray-600"><svg class="inline-block mr-2 bg-transparent" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="green" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="4"/><path d="M1.05 12H7M17.01 12h5.95"/></svg> <span>New</span></li>
+            <li class="flex items-end text-xs text-gray-600"><svg class="inline-block mr-2" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="orange" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="4"/><path d="M1.05 12H7M17.01 12h5.95"/></svg> <span>Improvement</span></li>
             <li class="flex items-end text-xs text-gray-600"><svg class="inline-block mr-2 bg-transparent" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="blue" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="4"/><path d="M1.05 12H7M17.01 12h5.95"/></svg> <span>Content</span></li>
+            <li class="flex items-end text-xs text-gray-600"><svg class="inline-block mr-2 bg-transparent" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="red" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="4"/><path d="M1.05 12H7M17.01 12h5.95"/></svg> <span>Bug Fix</span></li>
         </ul>
     </div>
 </div>
@@ -36,7 +38,7 @@ export default {
     data() {
         return {
             isVisible: false
-        };
+        }
     },
     methods: {
         toggleVisibility() {

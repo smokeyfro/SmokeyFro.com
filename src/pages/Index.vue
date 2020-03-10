2365947ade9f2323874017633afe0250f428ed26
Chris Rault <chris@smokeyfro.com>
2020-03-10 20:54:19 +0200

    Home content tweaks (wip)

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,11 +4,12 @@
 		<div class="grid w-full gap-0 content md:h-screen">
 			<HomeImage />
 			<div class="content">
-				<h1><span class="block sm:hidden">Howdy. I'm Chris</span><span class="hidden sm:block">Hello. My name  <strong>Chris</strong></span></h1>
-				<p>I am an independent designer and front-end developer working remotely from the Garden Route of South Africa.</p>
-				<p>This is my home on the web where I showcase my work, share my themes, write tutorials and post updates via my journal.</p>
-				<p>As a designer who is passionate about writing clean, performant code, I am able to take an idea from conception to finished product and have worked with clients of all sizes bringing their ideas to life. Need help with yours?</p>
-				<p><g-link to="/work" class="btn btn-primary" title="View my work">View My Work</g-link> <span class="mx-2 text-sm">or</span> <g-link to="/contact" class="btn btn-secondary" title="Say hello">Say Hello</g-link></p>
+				<h1><span class="block sm:hidden">Howdy. I'm Chris</span><span class="hidden sm:block">Hello. My name  <strong>Chris Rault</strong></span></h1>
+				<p>I am an independent designer and front-end developer, working remotely from my farm in the Garden Route of South Africa.</p>
+				<p>This is my home on the web where I showcase my <g-link to="/work" class="link" title="View a selection of my work">work</g-link>, share my WordPress and Gridsome <g-link to="/themes" class="link" title="Browse my growing collection of WordPress and JAMstack themes">themes</g-link>, write web-dev <g-link to="/tutorials" class="link" title="Read my journal">tutorials</g-link> and post updates via my <g-link to="/journal" class="link" title="Check out my web development tutorials">journal</g-link>.</p>
+				<p>As a designer who codes, I am able to take an idea from conception to finished product and have worked with clients of all sizes bringing their ideas to life.</p>
+				<p><strong>Need help with yours?</strong></p>
+				<p><g-link to="/work/services" class="btn btn-primary" title="Explore my services">Explore My Services</g-link> <span class="mx-2 text-sm">or</span> <g-link to="/contact" class="btn btn-secondary" title="Say hello">Say Hello</g-link></p>
 			</div>
 		</div>
 		</template>

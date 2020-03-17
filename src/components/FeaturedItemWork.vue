27162dc6a0943948bf8792f7e901189f3306ff62
Chris Rault <chris@smokeyfro.com>
2020-03-17 18:23:57 +0200

    Removed the image dimensions

--- src/components/FeaturedItemWork.vue
+++ src/components/FeaturedItemWork.vue
@@ -1,7 +1,7 @@
 <template>
 	<article class="h-auto bg-gray-100 rounded-md md:rounded-none md:bg-transparent md:flex md:items-center md:justify-start md:item">
 		<g-link :to="`${post.path}`" v-if="post.image" class="block overflow-hidden border featured-image rounded-t-md md:mr-10 md:w-1/2">
-			<g-image :src="post.image.src" width="420" height="300" :alt="post.image.alt" />
+			<g-image :src="post.image.src" :alt="post.image.alt" />
 		</g-link>
 		<div class="p-6 pt-4 md:w-1/3 summary md:p-0">
 			<h2 class="mt-0 text-xl md:text-2xl lg:text-4xl">{{ post.title }}</h2>

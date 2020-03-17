b647959526cc3a191137f53c0849168b4eafa65d
Chris Rault <chris@smokeyfro.com>
2020-03-17 09:56:57 +0200

    Tweaked the farm album description

--- src/pages/about/Photos.vue
+++ src/pages/about/Photos.vue
@@ -3,7 +3,7 @@
 		<h1>Photos</h1>
 		<p>I've had a fascination with photography since a young age and while I'd never consider myself more than an amateur, it's a past time I enjoy and so I pursue it.</p>
 		<div class="mt-6 mb-0 photos-wrap md:mt-10 md:mb-20">
-			<AlbumsByCategory category="farm" title="Forest Glade Farm" summary="My small piece of paradise, where I live off-grid, grow my own food and slowly-but-surely, edge closer to being full self-sufficient." />
+			<AlbumsByCategory category="farm" title="Forest Glade Farm" summary="My small piece of paradise, where I live off-grid, grow my own food and slowly-but-surely edge closer to being fully self-sufficient." />
 			<!-- <AlbumsByCategory category="south-africa" title="South Africa" summary="Photos taken from around the farm." />
 			<AlbumsByCategory category="travel" title="Travel" summary="Photos taken from around the farm." /> -->
 		</div>

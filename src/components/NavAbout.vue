70191e9ab9bdc433d31bb8aff2c5bca70463308e
Chris Rault <chris@smokeyfro.com>
2020-03-22 18:57:14 +0200

    Revert "Testing without the lightbox"
    This reverts commit 4365ef487d9e50a7cffe09840f07f0dd7dffc12d.

--- src/components/NavAbout.vue
+++ src/components/NavAbout.vue
@@ -1,7 +1,7 @@
 <template>
     <nav class="flex nav secondary" role="navigation">
         <g-link to="/about" exact><span>About</span></g-link>
-        <!-- <g-link to="/about/photos"><span>Photos</span></g-link> -->
+        <g-link to="/about/photos"><span>Photos</span></g-link>
         <g-link to="/about/videos"><span>Videos</span></g-link>
         <g-link to="/about/library"><span>Library</span></g-link>
         <g-link to="/about/gear"><span>Gear</span></g-link>

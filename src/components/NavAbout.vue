4a94ad581d799bc2cc26ece93c08d2bc1df305a7
Chris Rault <chris@smokeyfro.com>
2020-03-22 19:03:06 +0200

    Trigger rebuild

--- src/components/NavAbout.vue
+++ src/components/NavAbout.vue
@@ -1,10 +1,10 @@
 <template>
     <nav class="flex nav secondary" role="navigation">
-        <g-link to="/about" exact><span>About</span></g-link>
-        <g-link to="/about/photos"><span>Photos</span></g-link>
-        <g-link to="/about/videos"><span>Videos</span></g-link>
-        <g-link to="/about/library"><span>Library</span></g-link>
-        <g-link to="/about/gear"><span>Gear</span></g-link>
-        <g-link to="/about/credits"><span>Credits</span></g-link>
+        <g-link to="/about" exact title="About me"><span>About</span></g-link>
+        <g-link to="/about/photos" title="Photos"><span>Photos</span></g-link>
+        <g-link to="/about/videos" title="Videos I've made"><span>Videos</span></g-link>
+        <g-link to="/about/library" title="Reading list"><span>Library</span></g-link>
+        <g-link to="/about/gear" title="My gear list"><span>Gear</span></g-link>
+        <g-link to="/about/credits" title="Site credits"><span>Credits</span></g-link>
     </nav>
 </template>

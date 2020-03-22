6b1015b62d9a5075517ae638c47d889456dbbf87
Chris Rault <chris@smokeyfro.com>
2020-03-22 20:28:07 +0200

    Release notes key spacing

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -26,7 +26,7 @@
 							 	<!-- </vue-custom-scrollbar> -->
 							</div>
 							<div class="flex items-start self-end justify-start w-full p-4 mt-auto bg-gray-100">
-								<strong class="mt-1 text-xs uppercase">Key:</strong>
+								<strong class="mt-1 mr-3 text-xs uppercase">Key:</strong>
 								<ul class="grid w-full grid-cols-1 reset md:grid-cols-2">
 									<li class="flex items-end text-xs text-gray-600"><svg class="inline-block mr-2 bg-transparent" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="green" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="4"/><path d="M1.05 12H7M17.01 12h5.95"/></svg> <span>New</span></li>
 									<li class="flex items-end text-xs text-gray-600"><svg class="inline-block mr-2" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="orange" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="4"/><path d="M1.05 12H7M17.01 12h5.95"/></svg> <span>Improvement</span></li>
